8137e489b6c61cfea4fcda11bea34137d87807af
Romanos Trechlis <r.trechlis@gmail.com>
2022-01-24 18:23:31 +0200

    Action inputs (#3)
    * fixing warnings from linter
    
    * add action.yaml
    use input parameters instead of env
    
    * fix windows zip
    
    * debug
    
    * debug
    
    * action inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 set -eux
 
 BASE_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}"
-CREATE_BODY="{\"tag_name\": \"${RELEASE_NAME}\"}"
+CREATE_BODY="{\"tag_name\": \"${INPUT_RELEASE_NAME}\"}"
 
-beginswith() { 
+beginsWith() {
     case $2 in "$1"*) true;; *) false;; esac; 
 }
 
@@ -23,12 +23,11 @@ setupGo() {
         GO_URL="https://go.dev/dl/go1.14.15.linux-amd64.tar.gz"
     elif [ "${GO_VERSION}" = "1.13" ]; then
         GO_URL="https://dl.google.com/go/go1.13.8.linux-amd64.tar.gz"
-    elif beginswith https "${GO_VERSION}"; then
+    elif beginsWith https "${GO_VERSION}"; then
         GO_URL=${GO_VERSION}
     fi
 
-    echo $GO_URL
-    wget ${GO_URL} -O go-linux.tar.gz 
+    wget "${GO_URL}" -O go-linux.tar.gz
     tar -zxf go-linux.tar.gz
     mv go /usr/local/
     mkdir -p /go/bin /go/src /go/pkg
@@ -39,17 +38,17 @@ setupGo() {
 }
 
 getURLFromResponse() {
-    r=$(echo ${response} | tr '\r\n' ' ' | jq -c '.[]' |
-        while read i; do
-            test=$(echo ${i} | jq -r .tag_name);
-            if [ "$test" = "${RELEASE_NAME}" ]; then
-               res=`echo "$i" | jq -r .upload_url`
-               echo $res
+    r=$(echo "${response}" | tr '\r\n' ' ' | jq -c '.[]' |
+        while read -r i; do
+            test=$(echo "${i}" | jq -r .tag_name);
+            if [ "$test" = "${INPUT_RELEASE_NAME}" ]; then
+               res=$(echo "$i" | jq -r .upload_url)
+               echo "$res"
                break;
             fi
         done
     )
-    echo $r
+    echo "$r"
 }
 
 getUploadURL() {
@@ -61,33 +60,33 @@ getUploadURL() {
       -d "${CREATE_BODY}"
     )
 
-    exists=$(echo ${response} |  jq -r .errors[0].code)
+    exists=$(echo "${response}" |  jq -r .errors[0].code)
     if [ "${exists}" = "already_exists" ]; then 
         response=$(curl \
             -H "Authorization: Bearer ${GITHUB_TOKEN}" \
             -H "Accept: application/vnd.github.v3+json" \
             "${BASE_URL}/releases"
         )
-        UPLOAD_URL=$(getURLFromResponse $response)
-        N=`echo $UPLOAD_URL | sed 's/{?name,label}//g'`
-        echo $N
+        UPLOAD_URL=$(getURLFromResponse "$response")
+        N=$(echo "$UPLOAD_URL" | sed 's/{?name,label}//g')
+        echo "$N"
     else
-        UPLOAD_URL=`echo "${response}" | jq -r '.upload_url'`
-        N=`echo $UPLOAD_URL | sed 's/{?name,label}//g'`
-        echo $N
+        UPLOAD_URL=$(echo "${response}" | jq -r '.upload_url')
+        N=$(echo "$UPLOAD_URL" | sed 's/{?name,label}//g')
+        echo "$N"
     fi
 }
 
 PROJECT_ROOT="/go/src/github.com/${GITHUB_REPOSITORY}"
-PROJECT_NAME=$(basename $GITHUB_REPOSITORY)
-NAME="${NAME:-${PROJECT_NAME}_${RELEASE_NAME}}_${GOOS}_${GOARCH}"
+PROJECT_NAME=$(basename "$GITHUB_REPOSITORY")
+NAME="${NAME:-${PROJECT_NAME}_${INPUT_RELEASE_NAME}}_${INPUT_GOOS}_${INPUT_GOARCH}"
 
 setupGo
 
-mkdir -p $PROJECT_ROOT
-rmdir $PROJECT_ROOT
-ln -s $GITHUB_WORKSPACE $PROJECT_ROOT
-cd $PROJECT_ROOT
+mkdir -p "$PROJECT_ROOT"
+rmdir "$PROJECT_ROOT"
+ln -s "$GITHUB_WORKSPACE" "$PROJECT_ROOT"
+cd "$PROJECT_ROOT"
 go get -v ./...
 
 if [ -z "${CMD_PATH+x}" ]; then
@@ -96,45 +95,45 @@ if [ -z "${CMD_PATH+x}" ]; then
 fi
 
 EXT=''
-if [ "$GOOS" = "windows" ]; then
+if [ "$INPUT_GOOS" = "windows" ]; then
 EXT='.exe'
 fi
 
 
-go build "${CMD_PATH}"
+GOOS=${INPUT_GOOS} GOARCH=${INPUT_GOARCH} go build "${CMD_PATH}"
 FILE_LIST="${PROJECT_NAME}${EXT}"
 
-if [ -z "${EXTRA_FILES+x}" ]; then
-    EXTRA_FILES=""
+ls -l
+
+if [ -z "${INPUT_EXTRA_FILES+x}" ]; then
+    INPUT_EXTRA_FILES=""
 fi
 
-FILE_LIST="${FILE_LIST} ${EXTRA_FILES}"
-FILE_LIST=`echo "${FILE_LIST}" | awk '{$1=$1};1'`
+FILE_LIST="${FILE_LIST} ${INPUT_EXTRA_FILES}"
+FILE_LIST=$(echo "${FILE_LIST}" | awk '{$1=$1};1')
 
 ARCHIVE_EXT=".tar.gz"
-MEDIA_TYPE='application/gzip'
-if [ "$GOOS" = "windows" ]; then
+if [ "$INPUT_GOOS" = "windows" ]; then
     ARCHIVE_EXT=".zip"
-    MEDIA_TYPE='application/zip'
-    zip -9r ${NAME}${ARCHIVE_EXT} ${FILE_LIST}
+    zip -9r "${NAME}"${ARCHIVE_EXT} "${FILE_LIST}"
 else
-    tar cvfz ${NAME}${ARCHIVE_EXT} ${FILE_LIST}
+    tar cvfz "${NAME}"${ARCHIVE_EXT} "${FILE_LIST}"
 fi
 
-CHECKSUM=$(md5sum ${NAME}${ARCHIVE_EXT} | cut -d ' ' -f 1)
+CHECKSUM=$(md5sum "${NAME}"${ARCHIVE_EXT} | cut -d ' ' -f 1)
 
 UPLOAD_URL=$(getUploadURL)
 
 curl \
   -X POST \
-  --data-binary @${NAME}${ARCHIVE_EXT} \
+  --data-binary @"${NAME}"${ARCHIVE_EXT} \
   -H 'Content-Type: application/octet-stream' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   "${UPLOAD_URL}?name=${NAME}${ARCHIVE_EXT}"
 
 curl \
   -X POST \
-  --data $CHECKSUM \
+  --data "$CHECKSUM" \
   -H 'Content-Type: text/plain' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   "${UPLOAD_URL}?name=${NAME}_checksum.txt"
